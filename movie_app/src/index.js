89e165477e6479d16dadac94ada8c4fccbad2351
vipulverma146 <47173395+vipulverma146@users.noreply.github.com>
2020-12-29 01:14:35 +0530

    created store

--- movie_app/src/index.js
+++ movie_app/src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+
 import './index.css';
 import App from './Components/App';
+import movies from './reducers';
+
+
+
+const store=createStore(movies);
+console.log('Store',store);
+console.log('STATE',store.getState());
 
 ReactDOM.render(
   <React.StrictMode>
