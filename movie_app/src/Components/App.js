ec88eb4b6b1f47a3d49a943ac16a626a6ab39b95
vipulverma146 <47173395+vipulverma146@users.noreply.github.com>
2020-12-29 03:34:02 +0530

    Subscribe to the store

--- movie_app/src/Components/App.js
+++ movie_app/src/Components/App.js
@@ -3,7 +3,33 @@ import {data} from '../data';
 import Navbar from './Navbar';
 import MovieCard from './MovieCard'; 
 
-function App() {
+class App extends React.Component{ 
+  componentDidMount(){
+
+    const {store}=this.props;
+    // subscribing
+
+    store.subscribe(()=>{
+      console.log("Updated");
+      this.forceUpdate();  //use to forcefully update the app component
+
+    })
+
+    // make api call to get movies
+
+    // dispatch action
+    store.dispatch({
+      type:'ADD_MOVIES',
+      movies:data
+
+    });
+
+    console.log("STATE",store.getState());
+  }
+
+  render()
+  {
+    const movies=this.props.store.getState();
   return (
     <div className="App">
       <Navbar />
@@ -17,7 +43,7 @@ function App() {
 
         <div className="list">
 
-          {data.map((movie,index)=>(
+          {movies.map((movie,index)=>(
             <MovieCard  movie={movie} key={`movies-${index}`}/>
 
           ))}
@@ -27,6 +53,7 @@ function App() {
       </div>
     </div>
   );
+ }
 }
 
 export default App;
--- movie_app/src/index.js
+++ movie_app/src/index.js
@@ -10,20 +10,20 @@ import movies from './reducers';
 
 const store=createStore(movies);
 console.log('Store',store);
-console.log(' BEFORE STATE',store.getState());
+// console.log(' BEFORE STATE',store.getState());
 
-store.dispatch({
-  type:'ADD_MOVIES',
-  movies:[{
-    name:'Super Man'
-  }]
-})
-console.log(' AFTER STATE',store.getState());
+// store.dispatch({
+//   type:'ADD_MOVIES',
+//   movies:[{
+//     name:'Super Man'
+//   }]
+// })
+// console.log(' AFTER STATE',store.getState());
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>,
   document.getElementById('root')
 );
