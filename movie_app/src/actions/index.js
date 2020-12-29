d3fb0a5076059e1b4e8ad016d1983b2ef5050d5d
vipulverma146 <47173395+vipulverma146@users.noreply.github.com>
2020-12-29 14:34:42 +0530

    added functionality of changing tabs

--- movie_app/src/actions/index.js
+++ movie_app/src/actions/index.js
@@ -3,6 +3,8 @@
 export const ADD_MOVIES = 'ADD_MOVIES';
 export const ADD_FAVOURITE = 'ADD_FAVOURITE';
 export const REMOVE_FROM_FAVOURITES ='REMOVE_FROM_FAVOURITES';
+export const SET_SHOW_FAVOURITES ='SET_SHOW_FAVOURITES';
+
 
 
 
@@ -28,5 +30,13 @@ export function removeFromFavourites(movie) {
     return {
         type: REMOVE_FROM_FAVOURITES,
         movie
+    }
+}
+
+
+export function setShowFavourites(val) {
+    return {
+        type: SET_SHOW_FAVOURITES,
+        val
     }
 }
--- movie_app/src/reducers/index.js
+++ movie_app/src/reducers/index.js
@@ -1,8 +1,9 @@
-import { ADD_MOVIES, ADD_FAVOURITE, REMOVE_FROM_FAVOURITES } from '../actions';
+import { ADD_MOVIES, ADD_FAVOURITE, REMOVE_FROM_FAVOURITES, SET_SHOW_FAVOURITES} from '../actions';
 
 const initialMoviesState={
     list:[],
-    favourites:[]
+    favourites:[],
+    showFavourites:false
 }
 
 export default function movies(state=initialMoviesState,action){  // initially state is empty
@@ -34,6 +35,11 @@ export default function movies(state=initialMoviesState,action){  // initially s
                 ...state,
                 favourites:filteredArray
 
+            }
+        case SET_SHOW_FAVOURITES:
+            return{
+                ...state,
+                showFavourites:action.val
             }
         default:
             return state;
