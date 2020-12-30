cd65bfdb6d6faf7770775bba4e84374c31bd0aee
vipulverma146 <47173395+vipulverma146@users.noreply.github.com>
2020-12-30 21:01:04 +0530

    middleware added

--- movie_app/src/index.js
+++ movie_app/src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore,applyMiddleware } from 'redux';
 
 import './index.css';
 import App from './Components/App';
 import rootReducer from './reducers';
 
+// function logger(obj,next,action)-----> logger(obj)(next)(action)
 
+const logger=function ({dispatch,getState}){
+  return function(next){
+    return function(action){
+      // middleware code
+      console.log("ACTION_TYPE",action.type);
+      next(action);
+    }
+  }
+}
 
-const store=createStore(rootReducer);
+const store=createStore(rootReducer,applyMiddleware(logger));
 console.log('Store',store);
 // console.log(' BEFORE STATE',store.getState());
 
